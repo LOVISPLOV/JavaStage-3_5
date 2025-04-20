c796d920f9d2fb0129e62dce7530d7c65bf897a4
Alex <zb.alex@mail.ru>
2025-04-20 21:13:48 +0300

    return

--- src/task02_charcase/CharMain.java
+++ src/task02_charcase/CharMain.java
@@ -1,7 +1,30 @@
 package task02_charcase;
 
+import java.util.Scanner;
+
 public class CharMain {
+    public static int findSlash(String input) {
+        if (input == null || input.isEmpty()) {
+            return -1;
+        }
+
+        int index = input.indexOf('/');
+
+        return index;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String userInput = scanner.nextLine();
+
+        int slashIndex = findSlash(userInput);
+
+        if (slashIndex != -1) {
+            System.out.println("Индекс символа '/': " + slashIndex);
+        } else {
+            System.out.println("Символ '/' не найден.");
+        }
     }
 }
